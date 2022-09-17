7366322c9df8a33d12f9307e79f8e81390b150d6
Vishnu Prabhakar <vishnuprabhakar42@gmail.com>
2022-09-17 23:14:47 +0530

    LinkedListImplementation.cpp

--- LinkedListImplementation.cpp
+++ LinkedListImplementation.cpp
@@ -12,12 +12,12 @@ struct Node
         next=NULL;
     }
 };
-
+// how r u bro
 int main()
 {
     Node *head=new Node(10);
     head->next=new Node(20);
     head->next->next=new Node(30);
-    cout<<head->data<<"-->"<<head->next->data<<"-->"<<head->next->next->data;
+    cout<<head->data<<"-->"<<head->next->data<<"-->"<<head->next->next->data<<;
     return 0;
 }
